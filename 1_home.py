8868963b96dba9876b7fe433e21a80f76ad0cf40
MlMauriciolopes <mlsilva1990@gmail.com>
2023-09-29 21:27:15 -0300

    Update URL

--- 1_home.py
+++ 1_home.py
@@ -1,4 +1,3 @@
-# streamlit run C:\\Users\\mlsil\\OneDrive\\Documentos\\projetos\\fifa_dashboard\\1_home.py
 # imports
 import streamlit as st
 import pandas as pd 
@@ -7,7 +6,7 @@ from datetime import datetime
 
 # dataset + filtros
 if 'data' not in st.session_state:
-    df_data = pd.read_csv(r'C:\\Users\\mlsil\\OneDrive\\Documentos\\projetos\\fifa_dashboard\\dataset\\CLEAN_FIFA23_official_data.csv', index_col=0)
+    df_data = pd.read_csv('dataset/CLEAN_FIFA23_official_data.csv', index_col=0)
     df_data = df_data[df_data["Contract Valid Until"] >= datetime.today().year]
     df_data = df_data[df_data['Value(£)'] > 0]
     df_data = df_data.sort_values(by="Overall", ascending=False)
